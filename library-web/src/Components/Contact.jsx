a06ab964dab24861f07c2f74d6480beec1e538c2
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2024-09-03 22:44:23 +0530

    Aapka commit message yahan likhein

--- library-web/src/Components/Contact.jsx
+++ library-web/src/Components/Contact.jsx
@@ -1,24 +1,4 @@
-// import React from 'react';
 
-// const Contact = () => {
-//   return (
-//     <section id="contact" className="py-20 bg-gray-800 text-white text-center">
-//       <div className="container mx-auto px-4">
-//         <h2 className="text-3xl font-bold mb-6">Contact Me</h2>
-//         <form className="max-w-md mx-auto ">
-//           <input type="text" placeholder="Your Name" className="w-full mb-4 p-2 rounded text-black outline-none" />
-//           <input type="email" placeholder="Your Email" className="w-full mb-4 p-2 rounded text-black outline-none" />
-//           <textarea placeholder="Your Message" className="w-full mb-4 p-2 rounded outline-none" rows="4"></textarea>
-//           <button type="submit" className="bg-blue-500 text-white px-6 py-2 rounded-full  hover:bg-blue-600 transition duration-300">
-//             Send
-//           </button>
-//         </form>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Contact;
 
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
